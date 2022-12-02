391c872477c86e1c6fc6678463522976baebf2cd
Takashi Hirasawa <takashi.hira@gmail.com>
2022-12-02 15:17:27 +0900

    eleventy.jsも修正

--- .eleventy.js
+++ .eleventy.js
@@ -9,20 +9,28 @@ dayjs.locale('ja')
 
 module.exports = function(eleventyConfig) {
 
+  global.filters = eleventyConfig.javascriptFunctions;
+  eleventyConfig.setPugOptions({
+    filters: global.filters,
+    pretty : true
+  });
+
   // グロナビとかのリンクを作るのに良さそうなプラグイン
   eleventyConfig.addPlugin(pluginNavigation);
   // RSSフィード用だけど sitemap.xml 作るのにも使ってるプラグイン
   eleventyConfig.addPlugin(pluginRss);
 
   // JSと画像をoutput（_site）にコピー
-  eleventyConfig.addPassthroughCopy("src/assets/js");
-  eleventyConfig.addPassthroughCopy("src/assets/img");
+  // eleventyConfig.addPassthroughCopy("src/assets/js");
+  // eleventyConfig.addPassthroughCopy("src/assets/img");
 
   // favicon.ico をコピー
   // eleventyConfig.addPassthroughCopy("src/favicon.ico");
 
   // 自動リロードがうまく動かないのでSassファイルをコピーして対応
-  eleventyConfig.addPassthroughCopy({"src/assets/sass": "assets/css/_reload"});
+  // eleventyConfig.addPassthroughCopy({"src/assets/sass": "assets/css/_reload"});
+  // eleventyConfig.addPassthroughCopy("src/_headers");
+  eleventyConfig.addPassthroughCopy("src/_redirects");
 
   // Markdownの設定とかカスタマイズ
   let markdownLibrary = markdownIt({
@@ -32,41 +40,23 @@ module.exports = function(eleventyConfig) {
   });
   eleventyConfig.setLibrary("md", markdownLibrary);
 
-  // ブラウザシンクの設定
-  eleventyConfig.setBrowserSyncConfig({
-    callbacks: {
-      ready: function(err, bs) {
-
-        bs.addMiddleware("*", (req, res) => {
-          const content_404 = fs.readFileSync('_site/404.html');
-          // Add 404 http status code in request header.
-          res.writeHead(404, { "Content-Type": "text/html; charset=UTF-8" });
-          // Provides the 404 content without redirect.
-          res.write(content_404);
-          res.end();
-        });
-      }
-    },
-    open: 'external',
-    notify: false,
-    https: true
-  });
+  // ブラウザシンクの設定はgulpfile.jsに
 
   // 日付をいい感じに変換してくれるプラグイン
   eleventyConfig.addFilter("formatDate", (date,format) => {
     return dayjs(date).format(format)
   });
 
-  // HTMLをきれいに整形
-  eleventyConfig.addTransform('prettier', (content, outputPath) => {
-    if (outputPath.endsWith('.html')) {
-      return prettier.format(content, {
-        printWidth: 160,
-        parser: 'html'
-      })
-    }
-    return content
-  })
+  // HTMLをきれいに整形（重くなるから不使用）
+  // eleventyConfig.addTransform('prettier', (content, outputPath) => {
+  //   if (outputPath.endsWith('.html')) {
+  //     return prettier.format(content, {
+  //       printWidth: 380,
+  //       parser: 'html'
+  //     })
+  //   }
+  //   return content
+  // })
 
   return {
     // 出力設定
